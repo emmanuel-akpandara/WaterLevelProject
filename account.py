a81609a6078a16cd8a79138f7fe76c374171a0fe
Emmanuel Akpandara <r0879038@student.thomasmore.be>
2024-01-26 12:55:37 +0100

    newpush2

--- account.py
+++ account.py
@@ -6,21 +6,21 @@ from firebase_admin import auth
 from config import FIREBASE_PROJECT_ID, FIREBASE_PRIVATE_KEY_ID, FIREBASE_PRIVATE_KEY, FIREBASE_CLIENT_EMAIL, FIREBASE_CLIENT_ID, FIREBASE_AUTH_URI, FIREBASE_TOKEN_URI, FIREBASE_AUTH_PROVIDER_CERT_URL, FIREBASE_CLIENT_CERT_URL, FIREBASE_UNIVERSE_DOMAIN
 
 
-cred = credentials.Certificate({
-    "type": "service_account",
-    "project_id": FIREBASE_PROJECT_ID,
-    "private_key_id": FIREBASE_PRIVATE_KEY_ID,
-    "private_key": FIREBASE_PRIVATE_KEY,
-    "client_email": FIREBASE_CLIENT_EMAIL,
-    "client_id": FIREBASE_CLIENT_ID,
-    "auth_uri": FIREBASE_AUTH_URI,
-    "token_uri": FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_CERT_URL,
-    "client_x509_cert_url": FIREBASE_CLIENT_CERT_URL,
-    "universe_domain": FIREBASE_UNIVERSE_DOMAIN
-})
-
-# firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate({
+#     "type": "service_account",
+#     "project_id": FIREBASE_PROJECT_ID,
+#     "private_key_id": FIREBASE_PRIVATE_KEY_ID,
+#     "private_key": FIREBASE_PRIVATE_KEY,
+#     "client_email": FIREBASE_CLIENT_EMAIL,
+#     "client_id": FIREBASE_CLIENT_ID,
+#     "auth_uri": FIREBASE_AUTH_URI,
+#     "token_uri": FIREBASE_TOKEN_URI,
+#     "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_CERT_URL,
+#     "client_x509_cert_url": FIREBASE_CLIENT_CERT_URL,
+#     "universe_domain": FIREBASE_UNIVERSE_DOMAIN
+# })
+cred = credentials.Certificate('gitguardians-app-2e4d25999060.json')
+firebase_admin.initialize_app(cred)
 def app():
 # Usernm = []
     st.title('Welcome to the :violet[Water Guardian] portal :sunglasses:')
